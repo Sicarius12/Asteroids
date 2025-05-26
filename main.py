5f951d9a305506a2d8f4f7321d5333d0793e7596
Enea Didi <eneadidigit@proton.me>
2025-05-26 19:30:36 -0400

    Added tick for running the game on a set number of frames per seconds

--- main.py
+++ main.py
@@ -10,14 +10,20 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
-    while True:
+    clock = pygame.time.Clock()
+    dt = 0
+    running = True
+
+    # Game loop
+    while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
-
+                running = False
 
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
 
     pygame.quit()
     print(pygame.get_init())
